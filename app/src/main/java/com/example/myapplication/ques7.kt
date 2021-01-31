a7e7277654d5de09893ee673a0f84ac0418ffcbb
khushboo-kb <ramanimk@gmail.com>
2021-01-31 01:02:34 -0300

    all 10 questions

--- app/src/main/java/com/example/myapplication/ques7.kt
+++ app/src/main/java/com/example/myapplication/ques7.kt
@@ -3,9 +3,9 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class level1 : AppCompatActivity() {
+class ques7 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_level1)
+        setContentView(R.layout.activity_ques7)
     }
 }
